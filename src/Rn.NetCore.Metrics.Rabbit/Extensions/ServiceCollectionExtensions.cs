e35ba6cee43c2e464dceca144a72a2283f1351e9
Richard Niemand <niemand.richard@gmail.com>
2022-05-25 22:11:54 -0600

    Stubbing extensions

---
+++ src/Rn.NetCore.Metrics.Rabbit/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using Rn.NetCore.Metrics.Extensions;
+
+namespace Rn.NetCore.Metrics.Rabbit.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddRabbitMQMetrics(this IServiceCollection services)
+  {
+
+    
+
+    return services;
+  }
+}
